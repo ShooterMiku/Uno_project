a5c07f20d409594ac56eb00c7f1e8ab0ec24a246
ShooterMiku <40128804+ShooterMiku@users.noreply.github.com>
2023-01-16 20:31:06 +0100

    Adding protocol Enums

---
+++ src/main/java/server/BroadcastsEnum.java
@@ -0,0 +1,24 @@
+package server;
+
+public enum BroadcastsEnum {
+    START("START"),
+    UPDATE("UPDATE"),
+    END("END"),
+    WIN("WIN"),
+    TERMINATE("TERMINATE"),
+    FROM("FROM");
+
+    private String tag;
+
+    BroadcastsEnum(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
