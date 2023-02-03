5a31eb3a8a3b9b9eab3fe34114066b2f688e6d88
ShooterMiku <40128804+ShooterMiku@users.noreply.github.com>
2023-02-03 22:55:51 +0100

    Update the pre/post condition

--- src/main/java/src/server/TGameProcess.java
+++ src/main/java/src/server/TGameProcess.java
@@ -41,11 +41,21 @@ public class TGameProcess implements GameProcess {
 
     private int num;
 
+    /**
+     * Move card
+     *
+     * @param client is a player
+     */
     @Override
     public boolean isGaming(GameClient client) {
         return clients.values().contains(client) && state == 1;
     }
 
+    /**
+     * Move card
+     *
+     * @param client is a player
+     */
     @Override
     public void addPlayers(GameClient client) {
         clients.put(client.getClientId(), (MyGameClient) client);
@@ -54,12 +64,18 @@ public class TGameProcess implements GameProcess {
 
     }
 
+    /**
+     * Move card
+     *
+     * @param client is a player
+     * @param message is a String
+     */
     @Override
     public synchronized void onMessage(GameClient client, String message) {
 
         CommandsEnum command = CommandsParser.getCommand(message);
         if (command == null) {
-            //  无法识别命令
+            //  error
             errorMsg(client, ErrorCodeEnum.E3);
             return;
         }
@@ -67,29 +83,29 @@ public class TGameProcess implements GameProcess {
             case PLAY: {
                 String players = String.join(",", clients.keySet());
                 client.send(CommandsParser.merge(CommandsEnum.QUEUE.getCommand(), players));
-                //如果当前玩家数量足够,就开启游戏了
+                //enough player to start
                 if (clients.size() == num) {
                     startGameProcess();
                 }
 
                 return;
-                //操作
+                //operation
             }
             case MOVE: {
                 String action = CommandsParser.splitCommand(message)[1];
-                //当前发言用户
+
                 String curPlayer = rotaryTable.get(currentPosition);
-                //轮不到你
+                //not u turn
                 if (!curPlayer.equals(client.getClientId())) {
                     errorMsg(client, ErrorCodeEnum.E3);
                     return;
                 }
-                //玩家选择抽牌的话
+                //
                 if (action.equals("DRAW")) {
-                    //给玩家发牌
+                    //draw card
                     String curCards = genPlayerCurrentCards(client, 1);
                     client.send(CommandsParser.merge(CommandsEnum.HAND.getCommand(), curCards));
-                } else {//玩家出牌
+                } else {//place card
                     moveCard(client, action);
                     String collect = playerStacks.get(client.getClientId()).stream().map(Card::getCarStr).collect(Collectors.joining(","));
                     client.send(CommandsParser.merge(CommandsEnum.HAND.getCommand(), collect));
@@ -97,14 +113,14 @@ public class TGameProcess implements GameProcess {
                 notifyPlayersOperating();
                 return;
             }
-            //发送消息
+            //send message
             case CHAT: {
                 String msg = CommandsParser.splitCommand(message)[1];
                 sendChatMessage(client, msg);
                 return;
             }
             case TAKE: {
-                //没用到
+                //did not use
                 return;
             }
             case CHANGE: {
@@ -115,7 +131,11 @@ public class TGameProcess implements GameProcess {
             default:
         }
     }
-
+    /**
+     * Move card
+     *
+     * @param card is a card from player
+     */
     public boolean moveValid(Card nowCard) {
         if (placedCards.size()<1) {
             return true;
@@ -130,10 +150,10 @@ public class TGameProcess implements GameProcess {
     }
 
     /**
-     * 交换卡片
+     * exchange card
      *
-     * @param client           客户端
-     * @param targetPlayerName 目标玩家名称
+     * @param client
+     * @param targetPlayerName
      */
     public void changeCards(GameClient client, String targetPlayerName) {
         Collection<Card> cards = playerStacks.get(targetPlayerName);
@@ -146,80 +166,102 @@ public class TGameProcess implements GameProcess {
     }
 
     /**
-     * 发送聊天消息
+     * Send message
      *
-     * @param client 客户端
-     * @param msg    消息
+     * @param client is a player
+     * @param msg is a string
      */
     public void sendChatMessage(GameClient client, String msg) {
         for (MyGameClient player : clients.values()) {
-            //不发给自己
+            //
             if (player != client) {
                 player.send(CommandsParser.merge("FROM", client.getClientId(), msg));
             }
         }
     }
-
+    /**
+     * Move card
+     *
+     * @param client is a player
+     */
     public void handCard(GameClient client) {
 
         String collect = playerStacks.get(client.getClientId()).stream().map(Card::getCarStr).collect(Collectors.joining(","));
         client.send(CommandsParser.merge(CommandsEnum.HAND.getCommand(), collect));
     }
-
+    /**
+     * Move card
+     *
+     * @param client is a player
+     * @param error is listed
+     */
     public void errorMsg(GameClient client, ErrorCodeEnum error) {
         client.send(CommandsParser.merge(CommandsEnum.ERROR.getCommand(), error.getTag()));
     }
 
     /**
-     * 出牌
+     * Move card
+     *
+     * @param client is a player
+     * @param action is a action
      */
     public void moveCard(GameClient client, String action) {
         Collection<Card> cards = playerStacks.get(client.getClientId());
         Card card = null;
 
         for (Card item : cards) {
-            //识别的奥当前牌的信息
+            //current card number
             if (item.getCarStr().equals(action)) {
                 card = item;
                 break;
             }
         }
-        //  没有牌这张牌的异常
+        //
         if (card == null) {
             errorMsg(client, ErrorCodeEnum.E1);
             return;
         }
-        // 有效
+        // effective
         if(!moveValid(card)){
             errorMsg(client, ErrorCodeEnum.E2);
             return;
         }
-        //放置
+        //place
         placedCards.add(new Card(card.getType(), card.getColor(), card.getNumber()));
-        //告诉大家
+        //boardcast
         updateCard(client, action, card);
-        //减少玩家的牌
+        //remove from hand
         cards.remove(card);
-        //加玩家的分
+        //adding score
         playerScorePlus(client, 1);
 
 
-        //牌出完了宣布胜利
+        //Winner Decalar
         if (cards.size() == 0) {
             gameOver(client);
         }
 
     }
-
+    /**
+     * Update card on hand
+     *
+     * @param client is a player
+     * @param action is at list
+     * @param card is a card
+     */
     public void updateCard(GameClient client, String action, Card card) {
         for (GameClient player : clients.values()) {
-            //不发给自己
+
             if (player != client) {
                 player.send(CommandsParser.merge(BroadcastsEnum.UPDATE.getTag(), client.getClientId(), action));
             }
         }
     }
-
+    /**
+     * end game
+     *
+     * @param winner is a player
+     */
     public void gameOver(GameClient winner) {
         state = 2;
 
@@ -229,16 +271,16 @@ public class TGameProcess implements GameProcess {
             String collect = playerScore.values().stream().map(String::valueOf).collect(Collectors.joining(","));
             //
             client.send(CommandsParser.merge("WIN", players, collect));
-            // TODO: 2023/2/3 后续
+            // TODO:
 //            client.send(CommandsParser.merge("END",players,curCards));
         }
     }
 
     /**
-     * 开始游戏线程
+     * start game thread
      */
     public void startGameProcess() {
-        //开启转盘
+        //
         rotaryTable.addAll(clients.keySet());
         currentPosition = 0;
         state = 1;
@@ -246,9 +288,9 @@ public class TGameProcess implements GameProcess {
         String players = String.join(",", clients.keySet());
 
 
-        //给当前游戏玩家发送消息
+        //sending message
         for (MyGameClient client : clients.values()) {
-            //每个玩家发五张牌
+            //sending five cards to each player
             String curCards = genPlayerCurrentCards(client, 5);
             //
             client.send(CommandsParser.merge("START", players, curCards));
@@ -257,10 +299,10 @@ public class TGameProcess implements GameProcess {
     }
 
     /**
-     * next 操作
+     * next operation
      */
     public void notifyPlayersOperating() {
-        // 告诉玩家开始出牌
+        // tell player to place card
         GameClient client = clients.get(rotaryTable.get(currentPosition));
 //        client.send("");
         if (clockwise) {
@@ -277,10 +319,10 @@ public class TGameProcess implements GameProcess {
     }
 
     /**
-     * 创玩家当前的卡片
+     * create beginning cards
      *
-     * @param client 客户端
-     * @return {@link String}
+     * @param client is a player
+     * @return {@link String} stanard card string
      */
     public String genPlayerCurrentCards(GameClient client, int cardNum) {
         Collection<Card> cards = playerStacks.get(client.getClientId());
@@ -295,10 +337,10 @@ public class TGameProcess implements GameProcess {
     }
 
     /**
-     * 玩家得分+
+     * Adding score to player
      *
-     * @param player 球员
-     * @param score  分数
+     * @param player is a client
+     * @param score  is a int
      */
     public void playerScorePlus(GameClient player, int score) {
         String clientId = player.getClientId();
--- src/test/java/src/server/TGameProcessTest.java
+++ src/test/java/src/server/TGameProcessTest.java
@@ -1,17 +1,370 @@
 package src.server;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.apache.log4j.Logger;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 public class TGameProcessTest {
-   // @BeforeAll
-    //public TGameProcess game = new TGameProcess(2);
 
+    private Logger log = Logger.getLogger(this.getClass());
+    @BeforeAll
+    static void initAll() {
+    }
+    @BeforeEach
+    void init() {
+    }
 
+    @Test
+    @DisplayName("is Gaming")
+    public void isGaming(){
+        try {
+            log.info("Starting execution of isGaming");
+            boolean expectedValue=false;
+            GameClient client = null;
 
+            int numc=0;
 
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            boolean actualValue=tgameprocess.isGaming( client);
+            log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("update Card")
+    public void updateCard(){
+        try {
+            log.info("Starting execution of updateCard");
+            GameClient client = null;
+            String action="";
+            Card card = null;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.updateCard( client ,action ,card);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofupdateCard-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("game Over")
+    public void gameOver(){
+        try {
+            log.info("Starting execution of gameOver");
+            GameClient winner = null;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.gameOver( winner);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofgameOver-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("start Game Process")
+    public void startGameProcess(){
+        try {
+            log.info("Starting execution of startGameProcess");
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.startGameProcess();
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofstartGameProcess-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("notify Players Operating")
+    public void notifyPlayersOperating(){
+        try {
+            log.info("Starting execution of notifyPlayersOperating");
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.notifyPlayersOperating();
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofnotifyPlayersOperating-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("gen Player Current Cards")
+    public void genPlayerCurrentCards(){
+        try {
+            log.info("Starting execution of genPlayerCurrentCards");
+            String expectedValue="";
+            GameClient client = null;
+            int cardNum=0;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            String actualValue=tgameprocess.genPlayerCurrentCards( client ,cardNum);
+            log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("player Score Plus")
+    public void playerScorePlus(){
+        try {
+            log.info("Starting execution of playerScorePlus");
+            GameClient player = null;
+            int score=0;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.playerScorePlus( player ,score);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofplayerScorePlus-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("get State")
+    public void getState(){
+        try {
+            log.info("Starting execution of getState");
+            int expectedValue=0;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            int actualValue=tgameprocess.getState();
+            log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("get Player Num")
+    public void getPlayerNum(){
+        try {
+            log.info("Starting execution of getPlayerNum");
+            int expectedValue=0;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            int actualValue=tgameprocess.getPlayerNum();
+            log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("add Players")
+    public void addPlayers(){
+        try {
+            log.info("Starting execution of addPlayers");
+            GameClient client = null;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.addPlayers( client);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofaddPlayers-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("on Message")
+    public void onMessage(){
+        try {
+            log.info("Starting execution of onMessage");
+            GameClient client = null;
+            String message="";
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.onMessage( client ,message);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofonMessage-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("move Valid")
+    public void moveValid(){
+        try {
+            log.info("Starting execution of moveValid");
+            boolean expectedValue=false;
+            Card nowCard = null;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            boolean actualValue=tgameprocess.moveValid( nowCard);
+            log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("change Cards")
+    public void changeCards(){
+        try {
+            log.info("Starting execution of changeCards");
+            GameClient client = null;
+            String targetPlayerName="";
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.changeCards( client ,targetPlayerName);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofchangeCards-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("send Chat Message")
+    public void sendChatMessage(){
+        try {
+            log.info("Starting execution of sendChatMessage");
+            GameClient client = null;
+            String msg="";
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.sendChatMessage( client ,msg);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofsendChatMessage-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("hand Card")
+    public void handCard(){
+        try {
+            log.info("Starting execution of handCard");
+            GameClient client = null;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.handCard( client);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofhandCard-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("error Msg")
+    public void errorMsg(){
+        try {
+            log.info("Starting execution of errorMsg");
+            GameClient client = null;
+            ErrorCodeEnum error = null;
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.errorMsg( client ,error);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution oferrorMsg-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("move Card")
+    public void moveCard(){
+        try {
+            log.info("Starting execution of moveCard");
+            GameClient client = null;
+            String action="";
+
+            int numc=0;
+
+            TGameProcess tgameprocess  =new TGameProcess( numc);
+            tgameprocess.moveCard( client ,action);
+            Assertions.assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution ofmoveCard-"+exception,exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+    @AfterEach
+    void tearDown() {
+    }
+    @AfterAll
+    static void tearDownAll() {
+    }
 }
